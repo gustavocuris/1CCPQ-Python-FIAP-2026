e7ce64795242ff95841f71458120d37dd377eb17
gustavocuris <gustavo.c.francisco2007@gmail.com>
2026-03-16 19:47:41 -0300

    Initial commit

---
+++ aula02-variáveis-operadoras/aula02a-variáveis.py
@@ -0,0 +1,41 @@
+print('Olá, Mundo!')
+
+print(7 + 4)
+print('7' + '4')
+print('7 + 4')
+
+# isso é um comentário de 1 linha
+
+#VARIÁVEIS
+
+nome = 'GUSTAVO'
+idade = 18
+peso = 59.8
+
+print(nome, idade, peso)
+
+print(f'Olá,{nome}! Você tem {idade} anos e {peso} KG')
+
+#INPUTS --- SIMULAÇÃO DE UM FORMS NO CMD
+
+nome1 = input ("Digite seu nome:")
+idade1 = input ("Digite sua idade:")
+peso1 = input ("Digite seu peso:")
+
+print(nome1, idade1, peso1)
+
+#DESAFIO 1
+nome2 = input ("Digite seu nome:")
+print(f'BEM-VINDO {nome2}! ')
+
+#DESAFIO 2
+dia = input ("Digite seu DIA de Nascimento:")
+mes = input ("Digite seu MÊS de Nascimento:")
+ano = input ("Digite seu ANO de Nascimento:")
+print(f'SUA DATA DE NASCIMENTO É: {dia}/{mes}/{ano}')
+
+#DESAFIO 3
+
+n1 = int (input ("Digite o primeiro número:"))
+n2 = int (input ("Digite o segundo número:"))
+print(f'A soma desses números é: {n1 + n2}')
