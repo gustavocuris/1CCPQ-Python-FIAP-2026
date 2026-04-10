d77485654153687222bc3df7a2afa62f814ec6a3
gustavocuris <gustavo.c.francisco2007@gmail.com>
2026-04-09 21:30:54 -0300

    12345

---
+++ NanoCourse-Python-2026/BoasVindas.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+print(nome) # SEM ESCRITA + VALOR
+
+nome = input("Digite seu nome: ")
+print("Boas Vindas," + nome) # COM ESCRITA + VALOR - AMADOR
+
+nome = input("Digite seu nome: ")
+print(f"Boas Vindas, {nome}") # COM ESCRITA + VALOR - PROFISSIONAL
---
+++ NanoCourse-Python-2026/CalculadoraInteiros.py
@@ -0,0 +1,16 @@
+print("Boas vindas ao calculador de Inteiros")
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+som = n1 + n2
+sub = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+
+print(f"\n O  resultado da Soma é {som}")
+
+print(f"\n O  resultado da Subtração é {sub}")
+
+print(f"\n O  resultado da Multiplicação é {mult}")
+
+print(f"\n O  resultado da Divisão é {div}")
