87a84caae931cbbc5db91654e13894227be3c0ba
gustavocuris <gustavo.c.francisco2007@gmail.com>
2026-04-13 19:30:22 -0300

    12345

---
+++ EXERCICIOS-AULA-N1/ExercicioE.py
@@ -0,0 +1,8 @@
+num = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if num % num2 == 0:
+    print(f"Os números {num} e {num2}s ão Múltiplos!")
+
+else:
+    print(f"Os números {num} e {num2} não são múltiplos! ")
